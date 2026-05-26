fe341b0d419418e7ca33d5e87d7868dceaa0593e
EXPERTO ITSI <experto@ITSI.EDU>
2026-05-26 10:37:16 -0500

    [ADD] Añadiendo Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,21 @@ import 'package:app_academico/features/carrera/providers/carrera.provider.dart';
 import 'package:app_academico/features/documents/providers/document.provider.dart';
 import 'package:app_academico/features/students/providers/student.provider.dart';
 import 'package:app_academico/features/students/subject/providers/subject.provider.dart';
+import 'package:app_academico/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'app/app.widget.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async{
+  /// NECESARIO para Firebase
+  WidgetsFlutterBinding.ensureInitialized();
+
+  /// Inicializar Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(
     MultiProvider(
       providers: [
